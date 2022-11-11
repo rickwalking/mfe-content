9dd095d2d87dbee4e515f91cb49502761d8add6b
Pedro Marins <ph.marins@hotmail.com>
2022-11-11 16:20:51 -0300

    feat: add Loading indicator

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,12 @@
 import { createGenerateClassName, StylesProvider } from "@material-ui/core/styles";
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import AuthApp from "./components/AuthApp";
+
 import Header from "./components/Header";
-import MarketingApp from "./components/MarketingApp";
+import Progress from "./components/Progress";
+
+const MarketingApp = lazy(() => import("./components/MarketingApp"));
+const AuthApp = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'container',
@@ -15,10 +18,12 @@ function App() {
       <BrowserRouter>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthApp} />
+              <Route path="/" component={MarketingApp} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
