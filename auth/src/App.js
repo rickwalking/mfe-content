d44dfa8c19ed02ed53278c8fca6774e76805f072
Pedro Marins <ph.marins@hotmail.com>
2022-11-11 15:36:27 -0300

    feat: Auth App

---
+++ auth/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import SignUp from "./components/Signup";
+import SignIn from "./components/Signin";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'auth',
+});
+
+function App({ history }) {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin" component={SignIn} />
+            <Route path="/auth/signup" component={SignUp} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+}
+
+export default App;
